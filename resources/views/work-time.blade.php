418a485f2899ed8791c63ed08c484d433897bba8
PawelKuch <kucharski.pawel99@gmail.com>
2024-09-20 11:39:50 +0200

    HrApp

--- resources/views/work-time.blade.php
+++ resources/views/work-time.blade.php
@@ -72,7 +72,6 @@
     day: {{$currentMonth -> dayName}}
 
     <a href="/delete-all-worktimes"><button class="btn btn-dark">delete all worktimes</button></a>
-    <a href="/work-time/{{$userId}}/"
 </div>
 
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>

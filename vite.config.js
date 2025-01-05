45c2b2f4b1ff2f9b92e8732689b90c18f0c79999
PawelKuch <kucharski.pawel99@gmail.com>
2025-01-05 18:04:19 +0100

    xavier

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,11 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    server: {
+        host: true,
+        port: 5173,
+    },
+
     plugins: [
         laravel({
             input: ['resources/js/app.js', 'resources/js/leaves.js', 'resources/js/userLeaves.js',
